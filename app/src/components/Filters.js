3084103893d9d5c713c95edb870e74c161d56b1f
Brian <briantcheung@gmail.com>
2021-03-25 17:11:34 -0700

    Added dropdown bar for filters

--- app/src/components/Filters.js
+++ app/src/components/Filters.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Collapsible from "react-collapsible";
 import ElementFilter from "./filters/ElementFilter";
 import WeaponFilter from "./filters/WeaponFilter";
 import TalentFilter from "./filters/TalentFilter";
@@ -6,16 +7,18 @@ import BossMatFilter from "./filters/BossMatFilter";
 
 const Filters = (props) => {
     return (
-        <div className="filter-container">
-            <div className="filter-bar">
-                <ElementFilter setFilters={props.elementFilters} />
-                <WeaponFilter setFilters={props.weaponFilters} />
+        <Collapsible trigger="Show Filters" triggerWhenOpen="Hide Filters">
+            <div className="filter-container">
+                <div className="filter-bar">
+                    <ElementFilter setFilters={props.elementFilters} />
+                    <WeaponFilter setFilters={props.weaponFilters} />
+                </div>
+                <div className="filter-bar">
+                    <TalentFilter setFilters={props.talentFilters} />
+                    <BossMatFilter setFilters={props.bossMatFilters} />
+                </div>
             </div>
-            <div className="filter-bar">
-                <TalentFilter setFilters={props.talentFilters} />
-                <BossMatFilter setFilters={props.bossMatFilters} />
-            </div>
-        </div>
+        </Collapsible>
     )
 }
 
